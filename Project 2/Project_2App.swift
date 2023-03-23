af3eb134d0f0c1136054d29f15645aa02490d2ec
Davyd Kuleba <kulebadavid@gmail.com>
2023-03-22 20:41:13 -0400

    Initial Commit

---
+++ Project 2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project 2
+//
+//  Created by Давид Кулеба on 2023-03-22.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
